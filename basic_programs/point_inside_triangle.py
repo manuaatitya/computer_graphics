0aff6bd0da087bec36329ba4106b724462ae2a92
manuaatitya <ermanuaatitya@gmail.com>
2021-01-02 23:29:47 +0530

    Removed the helper code for program execution and left only the class

--- basic_programs/point_inside_triangle.py
+++ basic_programs/point_inside_triangle.py
@@ -65,17 +65,5 @@ class point_within_triangle:
         for i in range(3):
             print('The entered cordinates of the point {} in the triangle is {} {}'.format(i+1, self.triangle[i].x, self.triangle[i].y))
 
-# Invoke class to execute the function
-t = point_within_triangle()
-t.get_input_point()
-t.get_input_triangle()
-t.compute_cross_product()
-if(t.point_within_triangle):
-    print('Point lies within the triangle')
-elif(t.point_intersects_triangle):
-    temp = t.intersecting_edge
-    print('Point lies on edge connecting Point {} ({}, {}) and Point {} ({}, {}) of the triangle'.format(temp + 1, t.triangle[temp].x, t.triangle[temp].y, temp + 2, t.triangle[(temp + 1)%3].x, t.triangle[(temp + 1)%3].y))
-else:
-    print('Point lies outside the triangle')
 
 
